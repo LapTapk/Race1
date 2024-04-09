133c1ea5f70ebe3dfe2dc27158797f4b9443bb95
LapTapk <fret4657@gmail.com>
2024-04-09 14:47:26 +0300

    Fixed renderer

--- engine/src/renderer.cpp
+++ engine/src/renderer.cpp
@@ -1,31 +1,39 @@
 #include "renderer.h"
 #include "transform.h"
 #include "game.h"
+#include <cmath>
 
 Renderer::Renderer(GameObject* go, std::string path_to_img) : Component(go) {
     texture.loadFromFile(path_to_img);
     sprite.setTexture(texture);
+    sf::Vector2f tzise{ texture.getSize() };
+    sprite.setOrigin(tzise.x / 2, tzise.y / 2);
 }
 
 void Renderer::update() {
     Game* game{ Game::instance };
     Transform* transform{ go->transform };
 
+    int camera_rot{ game->camera->transform->global_rot() };
+    int rot{ transform->global_rot() + camera_rot };
+    sprite.setRotation(rot);
+
     sf::Vector3f pos3{ transform->global_pos() };
-    sf::Vector2u tsize = texture.getSize();
-    sf::Vector3f camera_pos{ game->camera->transform->global_pos() };
-    float posx = pos3.x + game->conf.window_size.first / 2 -
-        tsize.x / 2 - camera_pos.x;
-    float posy = pos3.y + game->conf.window_size.second / 2 -
-        tsize.y / 2 - camera_pos.y;
-    sprite.setPosition(posx, posy);
+    sf::Vector2f pos2(pos3.x, pos3.y);
+
+    sf::Vector3f camera_pos3{ game->camera->transform->global_pos() };
+    sf::Vector2f camera_pos2(camera_pos3.x, camera_pos3.y);
+
+    sf::Vector2f window_offset(
+        game->conf.window_size.first / 2, game->conf.window_size.second / 2);
+    sf::Vector2f pos{ pos2 - camera_pos2 + window_offset };
+    sprite.setPosition(pos);
+
 
     sf::Vector2f scale{ transform->global_scale() };
     float camera_height_coef{
         1.0f * game->conf.default_camera_offset / game->camera->transform->position.z };
     sprite.setScale(scale.x * camera_height_coef, scale.y * camera_height_coef);
-
-    sprite.setRotation(transform->global_rot());
 
     game->window.draw(sprite);
 }
--- scenes/default_scene.h
+++ scenes/default_scene.h
@@ -10,15 +10,12 @@
 void create_defualt_scene(GameObject*& scene, GameObject*& camera) {
     scene = new GameObject(nullptr);
     GameObject* penguin{ new GameObject(scene) };
-    penguin->transform->position.x = 100;
     camera = new GameObject(scene);
-    camera->transform->position = sf::Vector3f(0, 0, 10);
+    camera->transform->position.z = 10;
+    camera->transform->rotation = 90;
     new Renderer(penguin, "../assets/penguin.png");
     Mover* mover{ new Mover(penguin, 150) };
     new WASDMoverInput(penguin, mover);
-    GameObject* penguin1{ new GameObject(scene) };
-    penguin1->transform->position.x = -100;
-    new Renderer(penguin1, "../assets/penguin.png");
 }
 
 #endif
